72edb137a5e16866e43255f18213a8678839a410
Patricio <patoog58@gmail.com>
2022-09-02 17:34:38 -0300

    [ADD]Cambio en los estilos css / cambio en el html / agrego carpeta imagenes + img + vectores / cambios en footer.js y footer.css

--- loana-jeans/src/components/Footer.js
+++ loana-jeans/src/components/Footer.js
@@ -1,57 +1,60 @@
 import React from "react";
 import "../styles/footer.css";
-
+// import img1 from "./../assets/logo.png"
 function Footer() {
     return (
-<footer>
-            <div className="row primary">
-                <div className="column">
-                    <img src="img/logo.png" width="210px" height="190px" alt="Logo img"/>
-                </div>
-                <div className="column links">
-                    <h2>Productos</h2>
-                    <ul>
-                        <li>
-                            <a href="#">Jeans</a>
-                        </li>
-                        <li>
-                            <a href="#">Shorts</a>
-                        </li>
-                        <li>
-                            <a href="#">Camperas</a>
-                        </li>
-                        <li>
-                            <a href="#">Ofertas</a>
-                        </li>
-                    </ul>
-                </div>
-
-                <div className="column links">
-                    <ul>
-                        <li>
-                        <h2 className="aux">Contacto</h2>
-                            <p> 🗺️ Av. San Martín 1587, CABA,Buenos Aires</p>
-                            <p>📱(+54) 9 11-5674-1599</p>
-                        </li>
-                    </ul>
-                </div>
-
-                <div className="column links">
-                    <ul>
-                        <li>
-                            <h2>Seguinos</h2>
-                                <a href="#">Instagram</a>
-                        </li>
-
-                        <li>
-                            <a href="#">Facebook</a>
-                        </li>
-                    </ul>
-                </div>
+    <footer class="footer">
+        <div class="row primary">
+            <div class="column">
+                {/* hay que cargar la imagen desde la carpeta "assets" para que se vea */}
+                <img src="img/logo.png" width="210px" height="190px" alt="Logo img" />
             </div>
-                    <p>© Loana Jeans 2020 - 2022 . Todos los derechos reservados.</p>
+                <div class="column links">
+                    <h2 class="h2__titulo-seccion--productos">Productos</h2>
+                        <ul class="ul__list">
+                            <li class="li__list-links">
+                                <a href="#">Camperas</a>
+                            </li>
 
-            </footer>
+                            <li class="li__list-links">
+                                <a href="#">Jeans</a>
+                            </li>
+                            <li class="li__list-links">
+                                <a href="#">Shorts</a>
+                            </li>
+                            <li class="li__list-links">
+                                <a href="#">Ofertas</a>
+                            </li>
+                        </ul>
+                </div>
+
+                <div class="column links">
+                    <h2 class="h2__titulo-seccion--contacto">Contacto</h2>
+                        <ul class="ul__list">
+                            <li class="li__list-links">
+                                                            {/* hay que cargar la imagen desde la carpeta "assets" > "img_vectores" para que se vea */}
+                                <p class="footer__p-info"> <img src="img/img__vectores/Vector.png"  width="15px" alt="vector ubicacion" /> Av. San Martín 1587, CABA </p>
+                                                            {/* hay que cargar la imagen desde la carpeta "assets" > "img_vectores" para que se vea */}
+                                <p footer__p-info > <img src="img/img__vectores/Vector_w.png" width="17px" alt="vector whatsapp" /> (+54) 9 11-5674-1599 </p>
+                            </li>
+                        </ul>
+                </div>
+
+                <div class="column links">
+                    <h2 class="h2__titulo-seccion--seguinos">Seguinos</h2>
+                        <ul class="ul__list">
+                            <li class="li__list-links">
+                                <a href="#">Instagram</a>
+                            </li>
+
+                            <li class="li__list-links">
+                                <a href="#">Facebook</a>
+                            </li>
+                        </ul>
+                </div>
+                <p class="footer__p-info-copyright">© Loana Jeans 2020 - 2022 . Todos los derechos reservados.</p>
+            </div>
+    </footer>
     );
 }
 
